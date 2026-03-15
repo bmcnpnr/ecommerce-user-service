5bdf0a5be1e5a5dde3a693b206c643dbe9125912
Mete <burakmetecanpinar@gmail.com>
2026-03-15 18:28:33 +0300

    test: Add unit tests for UserController covering registration, login, and user retrieval endpoints.

--- src/test/java/com/ecommerce/user/controller/UserControllerTest.java
+++ src/test/java/com/ecommerce/user/controller/UserControllerTest.java
@@ -5,10 +5,10 @@ import com.ecommerce.user.exception.InvalidCredentialsException;
 import com.ecommerce.user.exception.UserAlreadyExistsException;
 import com.ecommerce.user.exception.UserNotFoundException;
 import com.ecommerce.user.service.UserService;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,8 +26,7 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
 
     @MockitoBean
     private UserService userService;
